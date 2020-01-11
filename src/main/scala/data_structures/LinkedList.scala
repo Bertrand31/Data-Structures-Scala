6c5fe3295cbf6c4f475c840a8aa9f7d045755de3
Bertrand31 <bertrandjun@gmail.com>
2020-01-11 19:54:32 +0100

    Add find method and tests to LinkedList impl

--- src/main/scala/data_structures/LinkedList.scala
+++ src/main/scala/data_structures/LinkedList.scala
@@ -6,8 +6,24 @@ sealed trait LinkedList[+A] {
 
   def +:[U >: A](elem: U): LinkedList[U] = Node(elem, this)
 
+  def find[U >: A](predicate: U => Boolean): Option[U] =
+    this match {
+      case Empty => None
+      case node: Node[A] =>
+        if (predicate(node.value)) Some(node.value)
+        else node.next.find(predicate)
+    }
+
   // def ++:[U >: A](seq: TraversableOnce[U]): LinkedList[U] = seq.foldLeft(this)(_ +: _)
 }
 
 final case class Node[A](value: A, next: LinkedList[A]) extends LinkedList[A]
 final case object Empty extends LinkedList[Nothing]
+
+object LinkedListTest extends App {
+
+  val list = Node("test", Node("foo", Node("bar", Empty)))
+  assert(list.find(_ == "bar").isDefined)
+  assert(list.find(_ == "test").isDefined)
+  assert(list.find(_ == "baz").isEmpty)
+}
