1f5f065ce0d8516fac25f175f0eee8b30785b6c4
Bertrand31 <bertrandjun@gmail.com>
2020-01-11 19:54:13 +0100

    Improve CountMinSketch code style

--- src/main/scala/data_structures/CountMinSketch.scala
+++ src/main/scala/data_structures/CountMinSketch.scala
@@ -8,7 +8,7 @@ final case class CountMinSketch[A](
   isEmpty: Boolean = true,
 ) {
 
-  private lazy val hashFunctions: LazyList[String => Int] =
+  private def hashFunctions: LazyList[String => Int] =
     (1 to sketch.length)
       .to(LazyList)
       .map(i => (str: String) => Math.abs(stringHash(str, hashSeed + i)) % sketch.head.length)
@@ -19,10 +19,7 @@ final case class CountMinSketch[A](
     val newSketch =
       this.sketch
         .zip(hashes)
-        .map(kv => {
-          val (row, hash) = kv
-          row.updated(hash, row(hash) + 1)
-        })
+        .map({ case (row, hash) => row.updated(hash, row(hash) + 1) })
     this.copy(
       sketch=newSketch,
       isEmpty=false,
@@ -45,30 +42,28 @@ final case class CountMinSketch[A](
 
 object CountMinSketch {
 
-  def apply[A](w: Int, d: Int): CountMinSketch[A] =
+  def apply[A](width: Int, depth: Int): CountMinSketch[A] =
     CountMinSketch[A](
-      sketch=Array.fill(d)(Array.fill(w)(0)),
+      sketch=Array.fill(depth)(Array.fill(width)(0)),
       hashSeed=scala.util.Random.nextInt,
     )
 }
 
-object CountMinSketchTest {
+object CountMinSketchTest extends App {
 
   import scala.util.Random
 
-  def main(args: Array[String]): Unit = {
-    val sketch = CountMinSketch[String](10, 10)
-    assert(sketch.isEmpty)
-    val un = sketch + "foo"
-    assert(!un.isEmpty)
-    val deux = un + "bar" + "foo"
-    println(deux occurences "bar")
-    println(deux occurences "foo")
+  val sketch = CountMinSketch[String](10, 10)
+  assert(sketch.isEmpty)
+  val un = sketch + "foo"
+  assert(!un.isEmpty)
+  val deux = un + "bar" + "foo"
+  println(deux occurences "bar")
+  println(deux occurences "foo")
 
-    val numbersSketch = CountMinSketch[Int](100, 100)
-    val indexedSeq = (1 to 10000).map(_ => Random.between(1, 10))
-    val populated = numbersSketch ++ indexedSeq
-    println(populated occurences 3)
-    println(indexedSeq.count(_ == 3))
-  }
+  val numbersSketch = CountMinSketch[Int](100, 100)
+  val indexedSeq = (1 to 10000).map(_ => Random.between(1, 10))
+  val populated = numbersSketch ++ indexedSeq
+  println(populated occurences 3)
+  println(indexedSeq.count(_ == 3))
 }
