e94bb2219e3e565b8080331fb7008e6ec5f9ddd7
Bertrand31 <bertrandjun@gmail.com>
2020-02-14 18:19:34 +0100

    Fix and improve BitSet, add cardinality method

--- src/main/scala/data_structures/BitSet.scala
+++ src/main/scala/data_structures/BitSet.scala
@@ -4,14 +4,17 @@ import cats.implicits._
 
 case class BitSet(nb: Long = 0) {
 
-  def +(number: Int): BitSet =
-    BitSet(this.nb | (1 << number))
+  def +(number: Long): BitSet =
+    BitSet(this.nb | (1L << number))
 
-  def -(number: Int): BitSet =
-    BitSet(this.nb & (~(1 << number)))
+  def `++`: IterableOnce[Long] => BitSet =
+    _.iterator.foldLeft(this)(_ + _)
 
-  def member(number: Int): Boolean =
-    (this.nb & (~(1 << number))) =!= this.nb
+  def -(number: Long): BitSet =
+    BitSet(this.nb & (~(1L << number)))
+
+  def member(number: Long): Boolean =
+    (this.nb & (~(1L << number))) =!= this.nb
 
   def toIndexedSeq: IndexedSeq[Int] =
     this.nb
@@ -19,15 +22,16 @@ case class BitSet(nb: Long = 0) {
       .reverse
       .zip(Iterator from 0)
       .collect({ case ('1', index) => index })
+
+  def cardinality: Int =
+    this.nb.toBinaryString.count(_ === '1')
 }
 
 object BitSetTests extends App {
 
-  val bs = BitSet()
-  println(bs.nb)
-  val bsWithThree = bs + 3
-  println(bsWithThree.nb)
-  println(bsWithThree member 4)
-  println(bsWithThree member 3)
-  println(bsWithThree.toIndexedSeq)
+  val bs = BitSet() + 62
+  assert(bs.member(62))
+  assert(!bs.member(64))
+  assert(bs.cardinality === 1)
+  assert(bs.toIndexedSeq == IndexedSeq(62))
 }
