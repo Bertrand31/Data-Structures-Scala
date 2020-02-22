cdc98e658c024590c5dd2a06d192c972510d0afa
Bertrand31 <bertrandjun@gmail.com>
2020-02-22 14:01:31 +0100

    Improve BitSet code style

--- src/main/scala/data_structures/BitSet.scala
+++ src/main/scala/data_structures/BitSet.scala
@@ -6,32 +6,26 @@ case class BitSet(words: Array[Long] = Array(0)) {
 
   import BitSet._
 
-  type Words = Array[Long]
-
-  private def addToWord(nb: Long, wordIndex: Int, words: Words): Words = {
+  def +(number: Long): BitSet = {
+    val wordIndex = getWordIndex(number)
     val newWords = {
       val overflow = (wordIndex + 1) - words.size
       if (overflow > 0) words ++ new Array[Long](overflow)
       else words
     }
-    val updatedWord = newWords(wordIndex) | (1L << nb - (wordIndex.toLong << 6L))
-    newWords.updated(wordIndex, updatedWord)
+    val updatedWord = newWords(wordIndex) | (1L << number - (wordIndex.toLong << 6L))
+    BitSet(newWords.updated(wordIndex, updatedWord))
   }
-
-  private def removeFromWord(nb: Long, wordIndex: Int, words: Words): Words = {
-    val updatedWord = words(wordIndex) & (~1L << nb)
-    val newWords = words.updated(wordIndex, updatedWord)
-    newWords.head +: newWords.tail.takeWhile(_ =!= 0)
-  }
-
-  def +(number: Long): BitSet =
-    BitSet(this.addToWord(number, getWordIndex(number), words))
 
   def `++`: IterableOnce[Long] => BitSet =
     _.iterator.foldLeft(this)(_ + _)
 
-  def -(number: Long): BitSet =
-    BitSet(this.removeFromWord(number, getWordIndex(number), words))
+  def -(number: Long): BitSet = {
+    val wordIndex = getWordIndex(number)
+    val updatedWord = words(wordIndex) & (~1L << number)
+    val newWords = words.updated(wordIndex, updatedWord)
+    BitSet(newWords.head +: newWords.tail.takeWhile(_ =!= 0))
+  }
 
   def contains(number: Long): Boolean = {
     val word = this.words(getWordIndex(number))
