3557f7c0ef45df7d73480d5e516054522732550e
Bertrand31 <bertrandjun@gmail.com>
2020-02-23 22:01:19 +0100

    Improve SimpleBitSet performance with bit manipulation

--- src/main/scala/data_structures/HashArrayMappedTrie/Simple32BitSet.scala
+++ src/main/scala/data_structures/HashArrayMappedTrie/Simple32BitSet.scala
@@ -12,16 +12,11 @@ final case class Simple32BitSet(bitset: Int = 0) {
   def contains(number: Int): Boolean =
     (bitset & (~(1 << number))) =!= bitset
 
-  def getPosition(number: Int): (Int, Boolean) = {
-    val binaryString = bitset.toBinaryString
-    val rightmostBits = binaryString.drop(binaryString.size - number + 1)
-    val isSet = rightmostBits.head === '1'
-    val position = rightmostBits.tail.count(_ === '1')
-    (position, isSet)
-  }
+  private def countOnesUpTo(maxBinaryDigit: Int): Int =
+    (0 until maxBinaryDigit).count(shift =>
+      (bitset & ~(1L << shift)) =!= bitset
+    )
 
-  private val IntBits = Int.MaxValue.toBinaryString.size
-
-  def toArray: IndexedSeq[Int] =
-    (0 to IntBits).filter(bit => (bitset & ~(1 << bit)) =!= bitset)
+  def getPosition(number: Int): (Int, Boolean) =
+    (countOnesUpTo(number), contains(number))
 }
