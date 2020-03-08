fa5f48a107c3d064169d015e8d4b97c1c6052035
Bertrand31 <bertrandjun@gmail.com>
2020-03-08 19:42:51 +0100

    Use bit shifting to count leading zeroes, improve overall code style

--- src/main/scala/data_structures/HyperLogLog.scala
+++ src/main/scala/data_structures/HyperLogLog.scala
@@ -4,53 +4,45 @@
 
 package data_structures
 
-import scala.annotation.tailrec
 import scala.util.hashing.MurmurHash3.stringHash
 import scala.math.Ordering.Double.TotalOrdering
+import cats.implicits._
 
 object HyperLogLog {
 
-  @tailrec
-  private def getMaxPrefixLength[A](list: List[A], longuestPrefix: Int = 1): Double = {
-    val head +: tail = list
-    val headPrefixLength =
-      stringHash(head.toString)
-        .toBinaryString
-        .reverse
-        .padTo(32, '0')
-        .reverse
-        .takeWhile(_ == '0')
-        .length + 1
-    val newLonguestPrefix = Math.max(longuestPrefix, headPrefixLength)
-    if (tail.isEmpty) newLonguestPrefix
-    else getMaxPrefixLength(tail, newLonguestPrefix)
-  }
+  def numberOfLeadingZeros(number: Int): Int =
+    (31 to 0 by -1)
+      .takeWhile(shift => ((1 << shift) | number) =!= number)
+      .size
+
+  private def getMaxPrefixLength[A]: List[A] => Double =
+    _.foldLeft(0)((acc, item) =>
+      acc max numberOfLeadingZeros(Math.abs(stringHash(item.toString)))
+    )
 
   private def powOfTwo: Double => Double = Math.pow(2, _)
 
-  private def getRegisterCardinality: List[_] => Double =
-    (getMaxPrefixLength(_: List[_])) andThen powOfTwo
+  private def getRegisterCardinality: List[_] => Double = getMaxPrefixLength >>> powOfTwo
 
-  private def harmonicMean(values: Seq[Double]): Double =
-    values.length / values.map(1 / _).sum
+  private def harmonicMean(values: Iterable[Double]): Double = values.size / values.map(1 / _).sum
 
   def getCardinality[A](bits: Int, list: List[A]): Double = {
     val numberOfRegisters = powOfTwo(bits)
     val registersSize = Math.ceil(list.length / numberOfRegisters).toInt
-    harmonicMean {
-      list
-        .sliding(registersSize, registersSize)
+    val registers = list.sliding(registersSize, registersSize)
+    harmonicMean(
+      registers
         .map(getRegisterCardinality)
-        .toIndexedSeq
+        .toArray
         .sorted
         .take(Math.ceil(numberOfRegisters * 0.7).toInt)
-    }
+    )
   }
 }
 
  object HyperLogLogApp extends App {
 
-  val randNumbers = (1 to 100000).map(_ => scala.util.Random.between(1, 10))
+  val randNumbers = (1 to 100000).map(_ => scala.util.Random.between(1, 1000))
   val estimatedCardinality = HyperLogLog.getCardinality(4, randNumbers.toList)
   println(estimatedCardinality)
  }
