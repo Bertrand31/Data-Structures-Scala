8cdee8905cfa0cf7b2b97cc40de183d5e54dfa60
Bertrand31 <bertrandjun@gmail.com>
2020-04-11 22:21:47 +0200

    Improve CompressedTrie code style

--- src/main/scala/data_structures/CompressedTrie/CompressedTrie.scala
+++ src/main/scala/data_structures/CompressedTrie/CompressedTrie.scala
@@ -1,5 +1,6 @@
 package data_structures
 
+import cats.implicits._
 import org.roaringbitmap.RoaringBitmap
 import Utils.AugmentedArray
 import TrieUtils.{getIndexesFromString, getStringFromIndexes}
@@ -8,70 +9,74 @@ import RoaringBitmapUtils.AugmentedBitmap
 final case class CompressedTrie(
   private val bitset: RoaringBitmap = new RoaringBitmap,
   private val children: Array[CompressedTrie] = new Array(0),
-  private val isWord: Boolean = false,
+  private val isPath: Boolean = false,
 ) {
 
-  def insert(chars: Seq[Int], trie: CompressedTrie): CompressedTrie = {
+  type Path = Vector[Int]
+
+  def insert(chars: Seq[Int]): CompressedTrie = {
     val head +: tail = chars
-    val (position, isSet) = trie.bitset.getPosition(head)
+    val (position, isSet) = this.bitset.getPosition(head)
     if (tail.isEmpty)
       if (isSet) {
-        val updatedChild = trie.children(position).copy(isWord=true)
-        val updatedChildren = trie.children.updated(position, updatedChild)
-        trie.copy(children=updatedChildren)
+        val updatedChild = this.children(position).copy(isPath=true)
+        val updatedChildren = this.children.updated(position, updatedChild)
+        this.copy(children=updatedChildren)
       } else {
-        val newBitset = trie.bitset + head
-        val newChild = new CompressedTrie(isWord=true)
-        val updatedChildren = trie.children.insertAt(position, newChild)
-        trie.copy(bitset=newBitset, children=updatedChildren)
+        val newBitset = this.bitset + head
+        val newChild = new CompressedTrie(isPath=true)
+        val updatedChildren = this.children.insertAt(position, newChild)
+        this.copy(bitset=newBitset, children=updatedChildren)
       }
     else
       if (isSet) {
-        val updatedChild = insert(tail, trie.children(position))
-        val updatedChildren = trie.children.updated(position, updatedChild)
-        trie.copy(children=updatedChildren)
+        val updatedChild = this.children(position).insert(tail)
+        val updatedChildren = this.children.updated(position, updatedChild)
+        this.copy(children=updatedChildren)
       } else {
-        val newBitset = trie.bitset + head
-        val newChild = insert(tail, new CompressedTrie)
-        val updatedChildren = trie.children.insertAt(position, newChild)
-        trie.copy(bitset=newBitset, children=updatedChildren)
+        val newBitset = this.bitset + head
+        val newChild = CompressedTrie().insert(tail)
+        val updatedChildren = this.children.insertAt(position, newChild)
+        this.copy(bitset=newBitset, children=updatedChildren)
       }
   }
 
-  def +(word: String): CompressedTrie = insert(getIndexesFromString(word), this)
+  def `+`: String => CompressedTrie = getIndexesFromString >>> this.insert
 
-  def ++(words: IterableOnce[String]): CompressedTrie = words.iterator.foldLeft(this)(_ + _)
+  def `++`: IterableOnce[String] => CompressedTrie = _.iterator.foldLeft(this)(_ + _)
 
-  def ++(trie: CompressedTrie): CompressedTrie = this ++ trie.toList
+  def merge: CompressedTrie => CompressedTrie = this ++ _.toList
 
-  private def keys(currentPrefix: Vector[Int]): List[Vector[Int]] = {
+  private def keys(currentPrefix: Path): List[Path] = {
     val words =
       (this.bitset.toList zip this.children)
         .flatMap({
           case (char, subTrie) => subTrie.keys(currentPrefix :+ char)
         })
-    if (this.isWord) currentPrefix +: words else words
+    if (this.isPath) currentPrefix +: words else words
   }
 
-  private def getNFirst(n: Int, prefix: Vector[Int], soFar: List[Vector[Int]] = List()): List[Vector[Int]] =
+  private def getNFirst(n: Int, prefix: Path, soFar: List[Path] = List()): List[Path] =
     if (soFar.size >= n) soFar
     else
       (this.bitset.toList zip this.children)
         .foldLeft(soFar)((acc, tpl) => {
           if (acc.size >= n) acc
-          val (char, subTrie) = tpl
-          val word = prefix :+ char
-          if (subTrie.isWord)
-            subTrie.getNFirst(n, word, word +: acc)
-          else
-            subTrie.getNFirst(n, word, acc)
+          else {
+            val (char, subTrie) = tpl
+            val word = prefix :+ char
+            if (subTrie.isPath)
+              subTrie.getNFirst(n, word, word +: acc)
+            else
+              subTrie.getNFirst(n, word, acc)
+          }
         })
 
   def getNBelow(n: Int, remainingChars: String, basePrefix: String): List[String] =
     if (remainingChars.isEmpty) {
-      val baseWord = getIndexesFromString(basePrefix)
-      val nFirstBelow = getNFirst(n, baseWord) map getStringFromIndexes
-      if (this.isWord) basePrefix +: nFirstBelow else nFirstBelow
+      val basePath = getIndexesFromString(basePrefix)
+      val nFirstBelow = getNFirst(n, basePath) map getStringFromIndexes
+      if (this.isPath) basePrefix +: nFirstBelow else nFirstBelow
     } else {
       val firstChar = remainingChars.head
       val (position, _) = this.bitset.getPosition(firstChar.toInt)
