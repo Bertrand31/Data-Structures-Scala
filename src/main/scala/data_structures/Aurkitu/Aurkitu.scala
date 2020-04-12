3bad7806c8ddc984b230ebbb7cbc32812618080c
Bertrand31 <bertrandjun@gmail.com>
2020-04-12 23:23:25 +0200

    Add new string search data structure, Aurkitu

---
+++ src/main/scala/data_structures/Aurkitu/Aurkitu.scala
@@ -0,0 +1,64 @@
+package data_structures
+
+import data_structures.BitSetContainer.{BitSet, BitSetBuilder}
+import BitSet._
+
+final case class Aurkitu(
+  private val words: Map[Int, String]               = Map.empty,
+  private val characterPositions: Map[Char, BitSet] = Map.empty,
+  private val index: Map[(Char, Int), BitSet]       = Map.empty,
+) {
+
+  def +(word: String): Aurkitu = {
+    val wordId = this.words.size
+    val newWords = words + (wordId -> word)
+    val pairs = word.zipWithIndex
+    val newCharacterPositions = pairs.foldLeft(this.characterPositions)((acc, pair) => {
+      val (char, charPosition) = pair
+      val newBitSet = acc.getOrElse(char, BitSetBuilder()) + charPosition
+      acc.updated(char, newBitSet)
+    })
+    val newIndex = pairs.foldLeft(this.index)((acc, pair) => {
+      val newBitSet = acc.getOrElse(pair, BitSetBuilder()) + wordId
+      acc.updated(pair, newBitSet)
+    })
+    Aurkitu(newWords, newCharacterPositions, newIndex)
+  }
+
+  def `++`: IterableOnce[String] => Aurkitu = _.iterator.foldLeft(this)(_ + _)
+
+  def searchChunk: String => Set[String] =
+    _
+      .map(char =>
+        characterPositions.get(char) match {
+          case None => Set[String]()
+          case Some(positions) =>
+            positions
+              .toList
+              .map(_.toInt)
+              .map((char, _))
+              .flatMap(this.index.get)
+              .flatMap(_.toArray)
+              .map(_.toInt)
+              .map(this.words)
+              .toSet
+        }
+      )
+      .reduce(_ intersect _)
+}
+
+object Aurkitu {
+
+  def apply(initialItems: IterableOnce[String]): Aurkitu =
+    new Aurkitu ++ initialItems
+
+  def apply(initialItems: String*): Aurkitu =
+    Aurkitu(initialItems)
+}
+
+object AurkituApp extends App {
+
+  val data = List("reprode", "production", "sares", "deprot")
+  val aurkitu = Aurkitu(data)
+  println(aurkitu searchChunk "prod")
+}
